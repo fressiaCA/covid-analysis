23aff1652fbf7cbc7f785e6a9ce3ba7c628a42db
Fressia Ccarhuas <fressia.ccarhuas@gmail.com>
2022-06-02 22:51:39 -0500

    add: Total cases vs population

--- COVID-DataExploration.sql
+++ COVID-DataExploration.sql
@@ -8,8 +8,18 @@ order by 1,2
 
 --Looking at Total Cases vs Total Deaths
 
---How many cases are there in this country and then how many deaths do they have for their entires cases
--- We want to know the percentage of people who are dying , who actually get infected 
-Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)
+----How many cases are there in this country and then how many deaths do they have for their entires cases
+----We want to know the percentage of people who are dying , who actually get infected 
+
+-- Shows likelihood of dying if you contract covid in yor country
+Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
 From CovidDB..CovidDeaths
+Where location like '%peru%'
+order by 1,2
+
+-- Looking at Total Cases vs Population
+---- It show us what percentage of population has gotten covid
+Select Location, date, population, total_cases, (total_cases/population)*100 AS DeathPercentage
+From CovidDB..CovidDeaths
+Where location like '%peru%'
 order by 1,2
